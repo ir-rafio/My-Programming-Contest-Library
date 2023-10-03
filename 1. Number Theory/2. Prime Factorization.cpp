abb16d60a3cc97428d5c74c473446b5d42a21f03
Md. Irfanur Rahman Rafio <irfanurrahman5@iut-dhaka.edu>
2023-10-04 00:07:01 +0600

    Prime factorization for large numbers

--- 1. Number Theory/2. Prime Factorization.cpp
+++ 1. Number Theory/2. Prime Factorization.cpp
@@ -1,18 +1,38 @@
-vector<pair<int,int>> factorize(int num)
+void factorize(int num, vector<pair<int,int>>& v)
 {
-	vector<pair<int,int>> v;
-
-	while(num>1)
-	{
-		int p=divisor[num];
-		v.push_back({p, 0});
-
-		while(num%p==0)
-		{
-			v.back().second++;
-			num/=p;
-		}
-	}
-
-	return v;
+    if(v.size()) v.clear();
+ 
+    for(auto p: Primes)
+    {
+        if(num<N) break;
+ 
+        if(p*p>num)
+        {
+            v.push_back({num, 1});
+            num=1;
+        }
+ 
+        else if(num%p==0)
+        {
+            v.push_back({p, 0});
+ 
+            while(num%p==0)
+            {
+                v.back().second++;
+                num/=p;
+            }
+        }
+    }
+ 
+    while(num>1)
+    {
+        int p=divisor[num];
+        v.push_back({p, 0});
+ 
+        while(num%p==0)
+        {
+            v.back().second++;
+            num/=p;
+        }
+    }
 }
