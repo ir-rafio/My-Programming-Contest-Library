cea7455316103e7f1a9b81f8fcd75ca3fcb5e9f3
Nasrum <nurnasrum@iut-dhaka.edu>
2023-10-31 14:39:45 +0600

    grundy

--- 7. Misc/grundy.cpp
+++ 7. Misc/grundy.cpp
@@ -1,17 +1,21 @@
+/*
+single pile game-> greedy or game dp
+multiple pile game and disjunctive(before playing, choose 1 pile) -> NIM game
+else-> Grundy(converts n any game piles to n NIM piles)
+-----------------------------------------------------------------------------
+grundy(x)->the smallest nonreachable grundy value
+-----------------------------------------------------------------------------
+there are n pile of games and k type of moves.
+if XOR(grundy(games)) == 0: losing state
+else winning state
+*/
 vector<int> moves, dp;
-
 int mex(vector<int> &a) {
     set<int> b(a.begin(), a.end());
     for (int i = 0; ; ++i)
         if (!b.count(i))
             return i;
 }
-
-/*
-there are k moves.
-if XOR(grundy(input)) == 0: losing state
-else winning state
-*/
 int grundy(int x) {
 	if (dp[x] != -1) return dp[x];
 	vector<int> reachable;
