47570794098899acde68e91511fc3b67ef5de726
Md. Irfanur Rahman Rafio <irfanurrahman5@iut-dhaka.edu>
2023-11-02 17:26:36 +0600

    Updated Ordered Set

--- 2. Data Structures/1. Ordered Set.cpp
+++ 2. Data Structures/1. Ordered Set.cpp
@@ -2,4 +2,6 @@
 #include <ext/pb_ds/tree_policy.hpp>
 using namespace __gnu_pbds;
   
-#define ordered_set tree<int, null_type,less<int>, rb_tree_tag,tree_order_statistics_node_update>
+// #define ordered_set tree<int, null_type,less<int>, rb_tree_tag, tree_order_statistics_node_update>
+template <typename T>
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
